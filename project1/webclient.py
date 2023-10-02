d0ecbe28e21b1c70e7f3c766c3cf069d58ec5fa8
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-02 09:26:37 -0700

    Sends request to server and receives response

--- project1/webclient.py
+++ project1/webclient.py
@@ -8,9 +8,15 @@ if len(sys.argv) < required_num_command_line_args:
     print("Usage: <address> [port number]")
 else:
     web_address = sys.argv[1]
+    req = "GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(web_address).encode("ISO-8859-1")
     port = default_port
     if len(sys.argv) > required_num_command_line_args:
         port = sys.argv[2]
     
     s = socket.socket()
     s.connect((web_address, port))
+    s.sendall(req)
+    d = s.recv(4096)
+    print(d)
+    if len(d) == 0:
+        print("complete")

e8ddde057005f8bfdd3b8ba41bac1e7670ae49f1
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-02 09:20:25 -0700

    Creates socket connection

--- project1/webclient.py
+++ project1/webclient.py
@@ -2,8 +2,15 @@ import socket
 import sys
 
 required_num_command_line_args = 2
+default_port = 80
 
 if len(sys.argv) < required_num_command_line_args:
     print("Usage: <address> [port number]")
 else:
-    ...
+    web_address = sys.argv[1]
+    port = default_port
+    if len(sys.argv) > required_num_command_line_args:
+        port = sys.argv[2]
+    
+    s = socket.socket()
+    s.connect((web_address, port))

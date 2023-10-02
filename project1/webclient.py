d4ec93e13a2047d34582083b7c537558656b0885
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-02 09:44:34 -0700

    Refactors client and adds constants

--- project1/webclient.py
+++ project1/webclient.py
@@ -1,22 +1,42 @@
 import socket
 import sys
 
-required_num_command_line_args = 2
-default_port = 80
+REQ_NUM_ARGS = 2
+DEFAULT_PORT = 80
+RECV_BUFFER_SIZE = 4096
+ENCODING = "ISO-8859-1"
 
-if len(sys.argv) < required_num_command_line_args:
+def parse_args(args, default_port, req_num_args):
+    web_address = args[1]
+    port = default_port
+    if len(args) > req_num_args:
+        port = args[2]
+    return web_address, port
+
+def build_request(web_address, encoding):
+    http_command = "GET / HTTP/1.1\r\n"
+    host = "Host: {}\r\n".format(web_address)
+    connection_command = "Connection: close\r\n\r\n"
+    req = http_command + host + connection_command
+    return req.encode(encoding)
+
+def receive_response(socket, recv_buff_size):
+    d = socket.recv(recv_buff_size)
+
+    while len(d) != 0:
+        print(d)
+        d = socket.recv(recv_buff_size)
+
+def run_client():
+        web_address, port = parse_args(sys.argv, DEFAULT_PORT, REQ_NUM_ARGS)
+        req = build_request(web_address, ENCODING)
+        s = socket.socket()
+        s.connect((web_address, port))
+        s.sendall(req)
+        receive_response(s, RECV_BUFFER_SIZE)
+        s.close()
+
+if len(sys.argv) < REQ_NUM_ARGS:
     print("Usage: <address> [port number]")
 else:
-    web_address = sys.argv[1]
-    req = "GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(web_address).encode("ISO-8859-1")
-    port = default_port
-    if len(sys.argv) > required_num_command_line_args:
-        port = sys.argv[2]
-    
-    s = socket.socket()
-    s.connect((web_address, port))
-    s.sendall(req)
-    d = s.recv(4096)
-    print(d)
-    if len(d) == 0:
-        print("complete")
+    run_client()

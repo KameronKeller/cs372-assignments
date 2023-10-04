794b1c62d72436d68d47a6ecc039f807812bc27b
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-04 10:14:29 -0700

    Implements ability for server to respond to requests

--- project1/webserver.py
+++ project1/webserver.py
@@ -12,34 +12,26 @@ def parse_args(args, default_port, req_num_args):
         port = args[1]
     return port
 
-# def build_request(web_address, encoding):
-#     http_header = "GET / HTTP/1.1\r\n"
-#     host = "Host: {}\r\n".format(web_address)
-#     connection_command = "Connection: close\r\n\r\n"
-#     req = http_header + host + connection_command
-#     return req.encode(encoding)
+def receive_request(socket, recv_buff_size, encoding):
+    d = socket.recv(recv_buff_size)
 
-def receive_request(socket, recv_buff_size):
-    socket.recv()
-
-    while len(d) != 0:
-        print(d)
-        d = socket.recv(recv_buff_size)
+    while "\r\n\r\n" not in d.decode():
+        d += socket.recv(recv_buff_size)
+    
+    response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 6\nConnection: close\n\nHello!"
+    socket.send(response.encode(encoding))
 
 def run_server():
-        port = parse_args(sys.argv, DEFAULT_PORT, REQ_NUM_ARGS)
+        port = int(parse_args(sys.argv, DEFAULT_PORT, REQ_NUM_ARGS))
         # req = build_request(web_address, ENCODING)
         s = socket.socket()
         s.bind(('', port))
         s.listen()
-        new_conn = s.accept()
-        new_socket = new_conn[0]  # This is what we'll recv/send on
-        receive_request(new_socket, RECV_BUFFER_SIZE)
-
-        # s.connect((web_address, port))
-        # s.sendall(req)
-        # receive_response(s, RECV_BUFFER_SIZE)
-        # s.close()
+        while True:
+            new_conn = s.accept()
+            new_socket = new_conn[0]  # This is what we'll recv/send on
+            receive_request(new_socket, RECV_BUFFER_SIZE, ENCODING)
+            new_socket.close()
 
 if len(sys.argv) < REQ_NUM_ARGS:
     print("Usage: [port number]")

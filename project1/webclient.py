ccf13bd60c4ad8b757164eba7a68abc32ce4cf73
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-04 11:13:58 -0700

    Sets max and min num args; Uses buffer for printing response

--- project1/webclient.py
+++ project1/webclient.py
@@ -1,7 +1,8 @@
 import socket
 import sys
 
-REQ_NUM_ARGS = 2
+MIN_NUM_ARGS = 2
+MAX_NUM_ARGS = 3
 DEFAULT_PORT = 80
 RECV_BUFFER_SIZE = 4096
 ENCODING = "ISO-8859-1"
@@ -9,9 +10,9 @@ ENCODING = "ISO-8859-1"
 def parse_args(args, default_port, req_num_args):
     web_address = args[1]
     port = default_port
-    if len(args) > req_num_args:
+    if len(args) == req_num_args:
         port = args[2]
-    return web_address, port
+    return web_address, int(port)
 
 def build_request(web_address, encoding):
     http_header = "GET / HTTP/1.1\r\n"
@@ -22,13 +23,16 @@ def build_request(web_address, encoding):
 
 def receive_response(socket, recv_buff_size):
     d = socket.recv(recv_buff_size)
+    receive_buffer = d
 
     while len(d) != 0:
-        print(d)
         d = socket.recv(recv_buff_size)
+        receive_buffer += d
+
+    print(receive_buffer.decode())
 
 def run_client():
-        web_address, port = parse_args(sys.argv, DEFAULT_PORT, REQ_NUM_ARGS)
+        web_address, port = parse_args(sys.argv, DEFAULT_PORT, MAX_NUM_ARGS)
         req = build_request(web_address, ENCODING)
         s = socket.socket()
         s.connect((web_address, port))
@@ -36,7 +40,7 @@ def run_client():
         receive_response(s, RECV_BUFFER_SIZE)
         s.close()
 
-if len(sys.argv) < REQ_NUM_ARGS:
+if len(sys.argv) != MIN_NUM_ARGS and len(sys.argv) != MAX_NUM_ARGS:
     print("Usage: <address> [port number]")
 else:
     run_client()

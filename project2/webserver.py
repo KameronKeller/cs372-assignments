813a1e1dbb45959031605b9e2f108a296fdb1b2f
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-11 14:50:13 -0700

    Incoming requests are passed to the request class, and the requested file is read from file

--- project2/webserver.py
+++ project2/webserver.py
@@ -1,11 +1,13 @@
 import socket
 import sys
+from request import Request
 
 MIN_NUM_ARGS = 1
 MAX_NUM_ARGS = 2
 DEFAULT_PORT = 28333
 RECV_BUFFER_SIZE = 4096
 ENCODING = "ISO-8859-1"
+ERROR_404 = "HTTP/1.1 404 Not Found"
 
 def parse_args(args, default_port, req_num_args):
     port = default_port
@@ -13,12 +15,27 @@ def parse_args(args, default_port, req_num_args):
         port = args[1]
     return int(port)
 
-def receive_request(socket, recv_buff_size, encoding):
-    d = socket.recv(recv_buff_size)
+def get_requested_file(filename):
+    try:
+        with open(filename) as file:
+            data = file.read()
+            return data
+    except:
+        return ERROR_404
 
-    while "\r\n\r\n" not in d.decode():
-        d += socket.recv(recv_buff_size)
+def receive_request(socket, recv_buff_size, encoding):
+    request_buffer = socket.recv(recv_buff_size)
+
+    while "\r\n\r\n" not in request_buffer.decode():
+        request_buffer += socket.recv(recv_buff_size)
     
+    # sample_text = "GET /dir/dir2/foo.txt HTTP/1.1\r\nHost: www.example.com\r\nConnection: close\r\n\r\n"
+
+    request = Request(request_buffer.decode())
+    request.parse_request()
+    file_data = get_requested_file(request.file_name)
+    print(file_data)
+
     response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 6\nConnection: close\n\nHello!"
     socket.send(response.encode(encoding))
 

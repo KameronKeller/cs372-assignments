565e0a6b141830d80cccb13b1771dd497810ffc7
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-18 14:20:01 -0700

    Fixes potential bug caused by datetime

--- project3/timeclient.py
+++ project3/timeclient.py
@@ -1,5 +1,5 @@
 import socket
-import datetime
+import time
 
 address = "time.nist.gov"
 port = 37
@@ -15,7 +15,7 @@ def system_seconds_since_1900():
     # Number of seconds between 1900-01-01 and 1970-01-01
     seconds_delta = 2208988800
 
-    seconds_since_unix_epoch = int(datetime.datetime.now().strftime("%s"))
+    seconds_since_unix_epoch = int(time.time())
     seconds_since_1900_epoch = seconds_since_unix_epoch + seconds_delta
 
     return seconds_since_1900_epoch

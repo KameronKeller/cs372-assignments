1a73f11689b4d9f933ff5101195925a57fadca84
Kameron Keller <mrkameronkeller@gmail.com>
2023-10-19 12:33:46 -0700

    Implements required functions

--- project3/wordclient.py
+++ project3/wordclient.py
@@ -4,10 +4,38 @@ import socket
 # How many bytes is the word length?
 WORD_LEN_SIZE = 2
 
+# Sets the buffer size for receiving data
+RECV_BUFFER_SIZE = 4096
+
 def usage():
     print("usage: wordclient.py server port", file=sys.stderr)
 
 packet_buffer = b''
+
+def get_word_length(packet_buffer):
+    word_length_bytes = packet_buffer[:WORD_LEN_SIZE]
+    return int.from_bytes(word_length_bytes)
+
+def complete_word_received(packet_buffer, word_length):
+    return len(packet_buffer) >= word_length + WORD_LEN_SIZE
+
+def has_complete_packet(packet_buffer):
+    # If the packet_buffer is less than the word length size, return false
+    if len(packet_buffer) < WORD_LEN_SIZE:
+        return False
+    # Otherwise, verify the complete word has been received
+    else:
+        word_length = get_word_length(packet_buffer)
+        return complete_word_received(packet_buffer, word_length)
+
+def extract_packet(packet_buffer):
+    word_length = get_word_length(packet_buffer)
+    total_packet_length = WORD_LEN_SIZE + word_length
+    return total_packet_length, packet_buffer[:total_packet_length]
+
+def clear_received_packet(total_packet_length):
+    global packet_buffer
+    packet_buffer = packet_buffer[total_packet_length:]
 
 def get_next_word_packet(s):
     """
@@ -22,7 +50,19 @@ def get_next_word_packet(s):
 
     global packet_buffer
 
-    # TODO -- Write me!
+    while True:
+        if has_complete_packet(packet_buffer):
+            total_packet_length, packet = extract_packet(packet_buffer)
+            clear_received_packet(total_packet_length)
+            return packet
+
+        data = s.recv(RECV_BUFFER_SIZE)
+
+        # If no data received, return None to signal a closed connection
+        if len(data) == 0:
+            return None
+        
+        packet_buffer += data
 
 
 def extract_word(word_packet):
@@ -35,7 +75,8 @@ def extract_word(word_packet):
     Returns the word decoded as a string.
     """
 
-    # TODO -- Write me!
+    word = word_packet[WORD_LEN_SIZE:]
+    return word.decode()
 
 # Do not modify:
 

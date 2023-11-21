f2d42578bbe10a2b74e30e45ead0741feb9445a1
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-21 12:18:32 -0800

    Implements server functionality

--- project8/select_server.py
+++ project8/select_server.py
@@ -6,9 +6,38 @@ import sys
 import socket
 import select
 
+RECV_BUFFER_SIZE = 4096
+
 def run_server(port):
-    # TODO--fill this in
-    pass
+
+    read_set = set()
+    listening_socket = socket.socket()
+    listening_socket.bind(('', port))
+    listening_socket.listen()
+    read_set.add(listening_socket)
+
+    print("waiting for connections")
+
+    while True:
+        ready_to_read, _, _ = select.select(read_set, {}, {})
+
+        for s in ready_to_read:
+            if s == listening_socket:
+                new_conn, _ = s.accept()
+                peername = new_conn.getpeername()
+                print("{}: connected".format(peername))
+                read_set.add(new_conn)
+        
+            else:
+                data = s.recv(RECV_BUFFER_SIZE)
+                data_length = len(data)
+
+                if data_length == 0:
+                    print("{}: disconnected".format(peername))
+                    read_set.remove(s)
+                else:
+                    print("{} {} bytes: {}".format(peername, data_length, data))
+
 
 #--------------------------------#
 # Do not modify below this line! #

ef898360f9a9eeb996517b3ef23d80d880d818f8
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-28 09:15:33 -0800

    Receives hello payload

--- final_project/chatserver.py
+++ final_project/chatserver.py
@@ -5,10 +5,23 @@ import select
 
 RECV_BUFFER_SIZE = 4096
 
+# def has_complete_packet(packet_buffer):
+#     if data_length >= 2:
+#         packet_size = int.from_bytes(data[0:2])
+#     if data_length == 2 + packet_size:
+#         print("full packet received")
+#         print(data)
+
+# def receive_packet(socket):
+#     packet_buffer = b''
+#     while True:
+#         if has_complete_packet(packet_buffer):
+
 def run_server(port):
 
     read_set = set()
     listening_socket = socket.socket()
+    listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listening_socket.bind(('', port))
     listening_socket.listen()
     read_set.add(listening_socket)
@@ -26,13 +39,18 @@ def run_server(port):
                 read_set.add(new_conn)
         
             else:
+                buffer = b''
                 data = s.recv(RECV_BUFFER_SIZE)
                 data_length = len(data)
+
 
                 if data_length == 0:
                     print("{}: disconnected".format(peername))
                     read_set.remove(s)
                 else:
+                    packet_length = data[0:2]
+                    print('aaaa')
+                    print(int.from_bytes(packet_length))
                     print("{} bytes: {}".format(data_length, data))
 
 def usage():

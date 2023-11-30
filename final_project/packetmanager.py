02ab8df030e133439d9a8899fd30105c9cca19f3
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-30 14:50:01 -0800

    Delete logging

--- final_project/packetmanager.py
+++ final_project/packetmanager.py
@@ -1,12 +1,10 @@
 import json
-import logging
 
 packet_buffer = b''
 
 class PacketManager:
 
     def __init__(self, packet_header_size, buffer_size):
-        logging.basicConfig(filename='packetmanager.log', encoding='utf-8', level=logging.DEBUG)
         self.packet_header_size = packet_header_size
         self.buffer_size = buffer_size
 
@@ -17,30 +15,7 @@ class PacketManager:
         # Otherwise, verify the complete word has been received
         else:
             payload_size = self.get_payload_size(packet_buffer)
-            logging.debug("---------")
-            logging.debug(packet_buffer)
-            logging.debug("payload_size: {}".format(payload_size))
-            logging.debug("len(packet_buffer): {}".format(len(packet_buffer)))
-            logging.debug("2 + payload_size: {}".format(self.packet_header_size + payload_size))
-            logging.debug("---------")
             return len(packet_buffer) >= payload_size + self.packet_header_size
-
-    # def has_complete_packet(self, packet_buffer):
-    #     if len(packet_buffer) < self.packet_header_size:
-    #         return False
-    #     else:
-
-    #     if len(packet_buffer) >= self.packet_header_size:
-    #         packet_size = int.from_bytes(packet_buffer[0:self.packet_header_size])
-    #         logging.debug("---------")
-    #         logging.debug(packet_buffer)
-    #         logging.debug("packet_size: {}".format(packet_size))
-    #         logging.debug("len(packet_buffer): {}".format(len(packet_buffer)))
-    #         logging.debug("2 + packet_size: {}".format(self.packet_header_size + packet_size))
-    #         logging.debug("---------")
-    #         return len(packet_buffer) == self.packet_header_size + packet_size
-    #     else:
-    #         return False
         
     def clear_received_packet(self, total_packet_length):
         global packet_buffer
@@ -59,22 +34,17 @@ class PacketManager:
         global packet_buffer
         while True:
             if self.has_complete_packet(packet_buffer):
-                logging.debug(packet_buffer)
                 total_packet_length, packet = self.extract_packet(packet_buffer)
                 self.clear_received_packet(total_packet_length)
-                logging.debug("packet")
-                logging.debug(packet)
                 return packet
 
             data = socket.recv(self.buffer_size)
-            logging.debug(data)
 
             # If disconnected
             if len(data) == 0:
                 return 0
             
             packet_buffer += data
-        # packet_buffer += data
     
     def get_payload(self, buffer):
         payload = buffer[self.packet_header_size:]

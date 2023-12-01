50aae86929480f2193c4d22f079927a8096996fe
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-30 16:36:11 -0800

    Clean up code and refactor

--- final_project/chatclient.py
+++ final_project/chatclient.py
@@ -1,13 +1,14 @@
 import sys
 import socket
 import threading
-import logging
 import time
 
 from chatuicurses import init_windows, read_command, print_message, end_windows
-from payload import HelloPayload, ClientToServerChatPayload
+from packet import HelloPacket, ClientToServerChatPacket
 from packetmanager import PacketManager
 
+# Time in seconds for client to automatically close when server disconnects
+SERVER_DISCONNECT_TIMEOUT = 3 
 PACKET_HEADER_SIZE = 2
 RECV_BUFFER_SIZE = 4096
 
@@ -15,6 +16,13 @@ def exit_client(s):
     s.close()
     end_windows()
     sys.exit()
+
+def shutdown_client(s):
+    print_message("*** Unable to send message ***")
+    print_message("*** Connection was closed by the server ***")
+    print_message("*** This client will exit in {} seconds ***".format(SERVER_DISCONNECT_TIMEOUT))
+    time.sleep(SERVER_DISCONNECT_TIMEOUT)
+    exit_client(s)
 
 def prepare_output(message):
     message_type = message["type"]
@@ -32,26 +40,23 @@ def receive_messages(**kwargs):
     s = kwargs["socket"]
     server_closed_trigger = kwargs["trigger"]
     packet_manager = PacketManager(PACKET_HEADER_SIZE, RECV_BUFFER_SIZE)
-    # server_closed_trigger.set()
 
     while True:
         message = packet_manager.receive_packet(s)
+
+        # If no message received, the server is shutdown
+        # Trigger the thread event to close the client
+        # Else, display the message in the TUI
         if not message:
-            # logging.debug(message)
             server_closed_trigger.set()
-            # logging.debug(server_closed_trigger.is_set())
         else:
-        # message = packet_manager.get_payload(data)
             output = prepare_output(message)
-            if len(message) > 0: # can this condition somehow be abstracted into the receive packets method?
-                print_message(output)
-            data = b''
+            print_message(output)
 
 def usage():
     print("usage: chatclient.py nickname host port", file=sys.stderr)
 
 def main(argv):
-    logging.basicConfig(filename='client.log', encoding='utf-8', level=logging.DEBUG)
     try:
         nickname = argv[1]
         host = argv[2]
@@ -60,47 +65,44 @@ def main(argv):
         usage()
         return 1
     
+    # Initialize the TUI
     init_windows()
 
-    # Make the client socket and connect
+    # Make the client socket and attempt to connect
     s = socket.socket()
     try:
         s.connect((host, port))
     except:
+        # Close client if unable to connect
         print("*** Unable to connect to the server ***")
         exit_client(s)
 
+    # Create an event to watch for a shutdown server
     server_closed_trigger = threading.Event()
+
+    # Create the message displaying thread
     receiver_thread = threading.Thread(target=receive_messages, kwargs={"socket":s, "trigger":server_closed_trigger}, daemon=True)
     receiver_thread.start()
 
-    hello_payload = HelloPayload(nickname)
-    hello_packet = hello_payload.build_packet()
-    s.sendall(hello_packet)
+    # Create and send hello packet to server
+    hello_packet = HelloPacket(nickname)
+    s.sendall(hello_packet.packet)
 
-    # Loop forever sending data at random time intervals
     while True:
         try:
             command = read_command("{}> ".format(nickname))
             if server_closed_trigger.is_set():
-                print_message("*** Unable to send message ***")
-                print_message("*** Connection was closed by the server ***")
-                print_message("*** This client will exit in 3 seconds. ***")
-                time.sleep(3)
-                exit_client(s)
+                shutdown_client(s)
         except:
             break
-        
 
-        chat_payload = ClientToServerChatPayload(command)
-        chat_packet = chat_payload.build_packet()
-        # logging.debug("main {}".format(server_closed_trigger.is_set()))
-        # print(chat_packet)
-        s.sendall(chat_packet)
+        # Create and send a chat packet
+        chat_packet = ClientToServerChatPacket(command)
+        s.sendall(chat_packet.packet)
+
+        # If the command is /q, exit
         if command == "/q":
             exit_client(s)
-
-
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))

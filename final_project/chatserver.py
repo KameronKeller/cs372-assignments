caddc2e46b98cac3ff7a503980e3de504479da1d
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-30 17:03:16 -0800

    Refactor code

--- final_project/chatserver.py
+++ final_project/chatserver.py
@@ -7,50 +7,45 @@ from packet import JoinPacket, ServerToClientChatPacket, LeavePacket
 PACKET_HEADER_SIZE = 2
 RECV_BUFFER_SIZE = 4096
 
-def prepare_response_to(s, message, nicknames):
+def prepare_response(s, message, nicknames):
     message_type = message["type"]
     packet = None
     is_disconnected = False
+
+    # Build a response based on the message type
     match message_type:
         case "hello":
-            join_message = JoinPacket(message["nick"])
-            packet = join_message.build_packet()
+            packet = JoinPacket(message["nick"])
             nicknames[s] = message["nick"]
-            # return join_packet
         case "chat":
             chat_message = message["message"]
+
+            # If the message is /q, set the is disconnect flag
             if chat_message == "/q":
-                leave_message = LeavePacket(nicknames[s])
-                packet = leave_message.build_packet()
-                nicknames.pop(s, None)
+                packet = LeavePacket(nicknames[s])
                 is_disconnected = True
             else:
-                chat_message = ServerToClientChatPacket(nicknames[s], message["message"])
-                packet = chat_message.build_packet()
-            # return chat_packet
-    return packet, is_disconnected
+                packet = ServerToClientChatPacket(nicknames[s], message["message"])
+
+    response = packet.packet
+    return response, is_disconnected
 
     
+def broadcast_chat(s, packet_buffers, nicknames):
+    # Get the buffer from this socket
+    buffer = packet_buffers[s]
 
-def broadcast_chat(s, packet_buffers, packet_manager, nicknames):
-    message = packet_buffers[s]
-    print(message)
-    # response = b''
-    # if message == 0:
-    #     print("MADE IT HERE")
+    # Prepare the response
+    response, is_disconnected = prepare_response(s, buffer, nicknames)
 
-    #     response = leave_packet
-    #     print(response)
-    # else:
-    response, is_disconnected = prepare_response_to(s, message, nicknames)
+    # Send out the response to all connected clients
     for s1 in packet_buffers.keys():
         s1.sendall(response)
 
+    # If the client is disconnected, remove it from the dicts
     if is_disconnected:
         packet_buffers.pop(s, None)
-    else:
-        # Reset the packet buffer after it has been sent
-        packet_buffers[s] = b''
+        nicknames.pop(s, None)
 
     return packet_buffers
 
@@ -59,51 +54,59 @@ def run_server(port):
 
     packet_manager = PacketManager(PACKET_HEADER_SIZE, RECV_BUFFER_SIZE)
 
+    # Create a set to hold sockets to listen to
     read_set = set()
+
+    # Create the listening socket
     listening_socket = socket.socket()
+
+    # Allow server to be reused on same port
     listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Setup the listening socket and add to read_set
     listening_socket.bind(('', port))
     listening_socket.listen()
     read_set.add(listening_socket)
 
     print("Waiting for connections...")
 
+    # Keeps track of each sockets buffer
     packet_buffers = {}
+
+    # Keeps track of each sockets nickname
     nicknames = {}
 
     while True:
+        # Retrieve the next socket that has data ready
         ready_to_read, _, _ = select.select(read_set, {}, {})
 
         for s in ready_to_read:
+
+            # Watch for new connections
             if s == listening_socket:
+                # Accept a new connection
                 new_conn, _ = s.accept()
                 peername = new_conn.getpeername()
                 print("{}: connected".format(peername))
+
+                # Keep track of the connected sockets
                 read_set.add(new_conn)
+
+                # Setup an empty buffer in the packet buffer
                 packet_buffers[new_conn] = b''
         
             else:
+                # Receive incoming data from a connection
                 data = packet_manager.receive_packet(s)
-                if data == 0:
+
+                # If there is no data, the client has disconnected
+                if not data:
+                    print("{}: disconnected".format(peername))
                     read_set.remove(s)
                 else:
                     packet_buffers[s] = data
-                    data_length = len(data)
-                    if len(data) > 0:
-                        packet_buffers = broadcast_chat(s, packet_buffers, packet_manager, nicknames)
+                    packet_buffers = broadcast_chat(s, packet_buffers, nicknames)
                     
-                    # If this is a hello packet:"
-                    # nicknames[s] = "placeholder nickname"
-
-
-                # if data_length == 0:
-                #     print("{}: disconnected".format(peername))
-                #     read_set.remove(s)
-                # else:
-                #     packet_length = packet_buffers[s][0:2]
-                #     print('aaaa')
-                #     print(int.from_bytes(packet_length))
-                #     print("{} bytes: {}".format(data_length, packet_buffers[s]))
 
 def usage():
     print("usage: chatserver.py port", file=sys.stderr)

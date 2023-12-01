38648650d20d0beaaa11ecba0dbed63d5f9bf030
Kameron Keller <mrkameronkeller@gmail.com>
2023-11-30 17:16:52 -0800

    Clean up formatting

--- final_project/packet.py
+++ final_project/packet.py
@@ -1,11 +1,11 @@
 import json
 
-class Packet:
 
+class Packet:
     def __init__(self, data):
         self.data = data
         self.payload = self.create_payload(self.data)
-        self.payload_size = (len(self.payload)).to_bytes(length=2, byteorder='big')
+        self.payload_size = (len(self.payload)).to_bytes(length=2, byteorder="big")
         self.packet = self.build_packet()
 
     def create_payload(self, data):
@@ -14,49 +14,33 @@ class Packet:
 
     def build_packet(self):
         return self.payload_size + self.payload
-    
-class HelloPacket(Packet):
 
+
+class HelloPacket(Packet):
     def __init__(self, nick):
-        self.data = {
-            "type": "hello",
-            "nick": nick
-        }
+        self.data = {"type": "hello", "nick": nick}
         Packet.__init__(self, self.data)
+
 
 class ClientToServerChatPacket(Packet):
-
     def __init__(self, message):
-        self.data = {
-            "type": "chat",
-            "message": message
-        }
+        self.data = {"type": "chat", "message": message}
         Packet.__init__(self, self.data)
+
 
 class ServerToClientChatPacket(Packet):
-
     def __init__(self, nick, message):
-        self.data = {
-            "type": "chat",
-            "nick": nick,
-            "message": message
-        }
+        self.data = {"type": "chat", "nick": nick, "message": message}
         Packet.__init__(self, self.data)
+
 
 class JoinPacket(Packet):
-
     def __init__(self, nick):
-        self.data = {
-            "type": "join",
-            "nick": nick
-        }
+        self.data = {"type": "join", "nick": nick}
         Packet.__init__(self, self.data)
 
-class LeavePacket(Packet):
 
+class LeavePacket(Packet):
     def __init__(self, nick):
-        self.data = {
-            "type": "leave",
-            "nick": nick
-        }
+        self.data = {"type": "leave", "nick": nick}
         Packet.__init__(self, self.data)
